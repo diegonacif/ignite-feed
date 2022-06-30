1828b636e130e47a9fb2549e1d2f7ee97ff87097
diegonacif <diegoreisnacif@gmail.com>
2022-06-30 19:37:50 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css";
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
   return (
     <>
       <Header />
-      <Post
-        author="Diego Nacif"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque molestias, incidunt ad neque eum iste consequatur reiciendis, autem suscipit vero architecto at nisi. Quos corporis delectus, tempora quaerat aspernatur dolore."
-      />
-      <Post
-        author="William Nacif"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Nacif"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque molestias, incidunt ad neque eum iste consequatur reiciendis, autem suscipit vero architecto at nisi. Quos corporis delectus, tempora quaerat aspernatur dolore."
+          />
+          <Post
+            author="William Nacif"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+
+
+
       
     </>
   )
