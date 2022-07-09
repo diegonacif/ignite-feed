b25db6bb85b8bf56ebd69c238a535c6aab289fb6
diegonacif <diegoreisnacif@gmail.com>
2022-07-09 20:14:55 -0300

    feat: comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -16,18 +16,30 @@ export function Post() {
       </header>
 
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
+        <p>Fala galeraa 👋</p>
 
-      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, </p>evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, </p>evento da Rocketseat. O nome do projeto é DoctorCare 🚀
 
-      <p>👉&nbsp;&nbsp;<a href="">jane.design/doctorcare</a></p>
+        <p>👉&nbsp;&nbsp;<a href="">jane.design/doctorcare</a></p>
 
-      <p>
-        <a href="">#novoprojeto</a> {' '}
-        <a href="">#nlw</a> {' '}
-        <a href="">#rocketseat</a>
-      </p>
+        <p>
+          <a href="">#novoprojeto</a> {' '}
+          <a href="">#nlw</a> {' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário"
+        />    
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 };
