ca7482f6dec573795a763353254e680bb22bfcee
diegonacif <diegoreisnacif@gmail.com>
2022-07-19 20:44:46 -0300

    feat: new comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -5,7 +6,19 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
+const comments = [
+  1,
+  2,
+
+]
+
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?!'
+  ])
+
+  const [newCommentText, setNewCommentText] = useState('');
+
   const formatedPublishedDate = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -14,6 +27,21 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    const newCommentText = event.target.comment.value
+   
+    setComments([...comments, newCommentText]);
+
+    setNewCommentText('');
+
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
 
   return (
     <article className={styles.post}>
@@ -34,29 +62,21 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>;
+            return <p key={line.content}><a href="#">{line.content}</a></p>;
           }
-        })}
-        {/* <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉&nbsp;&nbsp;<a href="">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="">#novoprojeto</a> {' '}
-          <a href="">#nlw</a> {' '}
-          <a href="">#rocketseat</a>
-        </p> */}
+        })}      
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
+          name="comment"
+          value={newCommentText}
           placeholder="Deixe um comentário"
+          onChange={handleNewCommentChange}
         />    
 
         <footer>
@@ -65,9 +85,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} content={comment} />
+        }) }
       </div>
     </article>
   );
